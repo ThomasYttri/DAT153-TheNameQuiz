bed8a6cac8b6e2762c32c96b06b8df5940e81f98
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-01-29 14:03:17 +0100

    Psuedo code for quiz

--- app/src/main/java/com/example/thenamequizapp/Activities/QuizActivity.java
+++ app/src/main/java/com/example/thenamequizapp/Activities/QuizActivity.java
@@ -1,8 +1,14 @@
-package com.example.thenamequizapp;
+package com.example.thenamequizapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.thenamequizapp.R;
+
+import java.util.List;
+
+import Classes.Person;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        /*
+
+        Hent fram lista med personer
+
+        Velg tilfeldig person fra lista
+
+        Hent bilde fra person
+
+        onclick sjekk edittext om navn er lik navn i person objekt (hugs unngå case senitivitet)
+        Legg person til i ny liste over personer som har blitt valgt
+
+        Gi tilbakemelding Rett / feil
+        Legg til counter på antall rett
+
+        Hent fram nytt bilde, sjekk mot personer som allerede er valgt
+
+        repeat
+
+         */
     }
 }
