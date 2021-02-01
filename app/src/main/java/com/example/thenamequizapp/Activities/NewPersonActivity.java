d65300d489b7378543a2a8ee856477675fc32718
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-01 17:13:37 +0100

    Removed unnecessary imports

--- app/src/main/java/com/example/thenamequizapp/Activities/NewPersonActivity.java
+++ app/src/main/java/com/example/thenamequizapp/Activities/NewPersonActivity.java
@@ -16,7 +16,6 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.thenamequizapp.Classes.Database;
