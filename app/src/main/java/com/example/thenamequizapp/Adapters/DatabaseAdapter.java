11eca442720ef90bfda59bdc1788cd0ef4c26c88
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-01 14:32:09 +0100

    Added seperate class for Adapter

--- app/src/main/java/com/example/thenamequizapp/Adapters/DatabaseAdapter.java
+++ app/src/main/java/com/example/thenamequizapp/Adapters/DatabaseAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,23 +32,23 @@ public class DatabaseAdapter extends ArrayAdapter<Person> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
 
-        convertView = layoutInflater.inflate(this.resources, parent, false);
+        view = layoutInflater.inflate(this.resources, parent, false);
 
         //Image
-        ImageView imageView = convertView.findViewById(R.id.imageViewPerson);
+        ImageView imageView = view.findViewById(R.id.imageViewPerson);
         imageView.setImageDrawable(getItem(position).getImage());
 
         //Name
-        TextView textView = convertView.findViewById(R.id.textViewName);
+        TextView textView = view.findViewById(R.id.textViewName);
         textView.setText(getItem(position).getName());
 
         //Delete button
-        ImageButton deleteButton = convertView.findViewById(R.id.deleteButton);
+        Button deleteButton = view.findViewById(R.id.deleteButton);
         deleteButton.setOnClickListener(v -> remove(getItem(position)));
 
-        return convertView;
+        return view;
     }
 }
