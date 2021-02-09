1559b8181e91a6e04dffa2816f03d61c6cd36ad1
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-09 21:50:16 +0100

    Added PersonDao

---
+++ app/src/main/java/com/example/thenamequizapp/DAO/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.thenamequizapp.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.thenamequizapp.Classes.Person;
+
+import java.util.List;
+
+@Dao
+public interface PersonDao {
+    @Query("SELECT * FROM person")
+    List<Person> getALl();
+
+    @Query("SELECT * FROM person WHERE uid IN (:personIds)")
+    List<Person> loadAllByIds(int[] personIds);
+
+    @Insert
+    void addPerson(Person person);
+
+    @Delete
+    void deletePerson(Person person);
+}
