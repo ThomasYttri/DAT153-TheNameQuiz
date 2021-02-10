d41965ca975d712ec42d91cfcc0701c870f8b8f9
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-10 13:11:27 +0100

    Implemented ActivityButtonTest

--- app/src/androidTest/java/com/example/thenamequizapp/ActivityButtonTest.java
+++ app/src/androidTest/java/com/example/thenamequizapp/ActivityButtonTest.java
@@ -1,14 +1,27 @@
 package com.example.thenamequizapp;
 
+import android.content.ComponentName;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import com.example.thenamequizapp.Activities.DatabaseActivity;
 import com.example.thenamequizapp.Activities.MainActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -21,9 +34,13 @@ public class ActivityButtonTest {
 
     @Test
     public void launchActivityTest(){
-
-
-
+        activityRule.getScenario();
+        // Init Intents
+        Intents.init();
+        // Perform onclick
+        onView(withId(R.id.mainDatabase)).perform(ViewActions.click());
+        // Compare Activity
+        intended(hasComponent(DatabaseActivity.class.getName()));
     }
 
 }
