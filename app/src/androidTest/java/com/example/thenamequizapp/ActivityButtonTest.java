bfe85414f48002aff066daf25e98d6f66de8a73a
Thomas Valvik Yttri <thoytt04@hotmail.com>
2021-02-10 13:24:30 +0100

    Updated ActivityButtonTest

--- app/src/androidTest/java/com/example/thenamequizapp/ActivityButtonTest.java
+++ app/src/androidTest/java/com/example/thenamequizapp/ActivityButtonTest.java
@@ -1,10 +1,7 @@
 package com.example.thenamequizapp;
 
-import android.content.ComponentName;
-
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -16,11 +13,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
